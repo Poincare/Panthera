bc263b5652d84791c023eb79e7d1a2872bfcaa13
Poincare <dhaivatpandya@gmail.com>
2014-02-15 15:20:39 -0600

    moved files

--- caches/ls_cache.go
+++
@@ -1,9 +0,0 @@
-package caches
-/*
-* This file implements a simple "ls" cache.
-* That means that we will cache "getListing"
-* calls that are supposed to go to the NameNode
-* and just return them to the client
-* as long as we know that no file changes have been made
-* through the NameNode or the DataNode.
-*/
