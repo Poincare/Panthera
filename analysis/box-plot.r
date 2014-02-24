46a96011f58523e60d30a32459e3e95f715ae057
Poincare <dhaivatpandya@gmail.com>
2014-02-24 00:58:22 -0600

    added poster

--- analysis/box-plot.r
+++ analysis/box-plot.r
@@ -6,7 +6,7 @@ noncached_data = unlist(na.omit(dat[2]))
 
 df <- data.frame(metacached = cached_data, non_metacached = noncached_data)
 pdf("boxplot.pdf")
-boxplot(df, ylim=c(0, 40000000), col=c('cornflowerblue', 'chocolate1'), xaxt="n")
+boxplot(df, ylim=c(0, 4000000), col=c('cornflowerblue', 'chocolate1'), xaxt="n")
 title("Metacached (Panthera) vs Non-metacached")
 axis(1, at=1:2, labels=c("metacached (panthera)", "non-metacached"))
 title(ylab = "Access Time (nanoseconds, lower is better)")
