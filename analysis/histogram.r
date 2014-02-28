04a6c57aea77fb8edcc1cda472f733f6021aec42
Poincare <dhaivatpandya@gmail.com>
2014-02-28 14:37:55 -0600

    added distribution for cached latency

--- analysis/histogram.r
+++ analysis/histogram.r
@@ -4,6 +4,6 @@ cached_latency = unlist(na.omit(dat[1]))
 no_cached_latency = unlist(na.omit(dat[2]))
 
 pdf("histogram.pdf")
-hist(no_cached_latency, breaks=1000, col='chocolate1', xlim=c(0, 16000000), ylim=c(0, 250))
-hist(cached_latency, breaks=100, col='cornflowerblue', add=T)
+hist(no_cached_latency, breaks=1000, col='chocolate1', xlim=c(0, 1600000), ylim=c(0, 25))
+hist(cached_latency, breaks=10, col='cornflowerblue', add=T)
 dev.off()
